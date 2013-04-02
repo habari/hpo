841324c21d1aaf0781b7e207f6254470acb9fda4
Chris J. Davis <chrisjdavis@contrib.habariproject.org>
2013-04-01 21:45:21 -0500

    Add some more hotness to the single view.

--- blog.single.php
+++ blog.single.php
@@ -1,6 +1,17 @@
 <?php namespace Habari; ?>
 <?php $theme->display('header'); ?>
-<div id="introduction">
+<div id="intro" class="subpage">
+	<div class="bg"></div>
+	<div class="container">
+		<h1>News from Habari</h1>
+	</div>
+</div>
+<div id="intro_header">
+	<div class="container">
+		<h3>Updates, Tutorials &amp; Articles from the Habari Community.</h3>
+	</div>
+</div>
+<div id="article" class="contributing">
 	<div class="container">
 		<div class="row offset-by-one">
 			<div id="community" class="fourteen area columns">
