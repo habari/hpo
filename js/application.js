0fa7c5c3613e9bea52d6041a2f39588ccedab8a5
ringmaster <a_github@midnightcircus.com>
2013-04-02 15:52:45 +0000

    Alter theme.xml for area, and fix application.js

--- js/application.js
+++ js/application.js
@@ -4,10 +4,14 @@ $(document).ready(function() {
 
 var positionFooter = function() {
 	var docHeight = $(window).height();
-	var footerHeight = $('#ending').height();
-	var footerTop = $('#ending').position().top + footerHeight;   
+	var ft_el = $('#ending');
+	if(!ft_el.length) {
+		ft_el = $('body > footer');
+	}
+	var footerHeight = ft_el.height();
+	var footerTop = ft_el.position().top + footerHeight;   
 	if (footerTop < docHeight) {
-		$('#ending').css('margin-top', (docHeight - footerTop) + 'px');
+		ft_el.css('margin-top', (docHeight - footerTop) + 'px');
 	}
 }
 
