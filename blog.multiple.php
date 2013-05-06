5d45575bcee10e39b57f574c20a994382ca548ea
Chris J. Davis <chrisjdavis@contrib.habariproject.org>
2013-05-06 11:36:18 -0500

    A number of updates, including pagintation for the news section and working search. I am tracking down two small errors currently. 1. For some reason the pager isn't working 100% on news, and 2. When API docs are returned in the search the URIs are broken since we also need to pass the document they belong to for the URI to be generated.

--- blog.multiple.php
+++ blog.multiple.php
@@ -17,13 +17,13 @@
 		<div id="page-selector" class="top sixteen columns">
 			<?php echo $theme->prev_page_link(); ?> <?php echo $theme->page_selector( null, array( 'leftSide' => 2, 'rightSide' => 2 ) ); ?> <?php echo $theme->next_page_link(); ?>
 		</div>
-		<div class="row offset-by-one">
+		<div class="row">
 			<?php foreach( $posts as $post ) { ?>
-			<div id="entry-<?php $post->id; ?>" class="fourteen area columns news_block">
+			<div id="entry-<?php $post->id; ?>" class="sixteen area columns news_block">
 				<div class="two columns">
 					<i class="icon-code">Q</i>
 				</div>
-				<div class="columns eleven body">
+				<div class="columns twelve body">
 					<h2><a href="<?php URL::out('display_single', array('slug' => $post->slug) ); ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h2>
 					<?php echo strip_tags( $post->content_excerpt ); ?>
 				</div>
